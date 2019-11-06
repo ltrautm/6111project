085d8a636e62074ed5d4cb273f89409ad26769f8
Leilani A Trautman <trautman@mit.edu>
2019-11-06 14:07:24 -0500

    created vivado project

---
+++ sos/sos.srcs/sources_1/new/servo.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/06/2019 02:03:42 PM
+// Design Name: 
+// Module Name: servo
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module servo(
+
+    );
+endmodule
