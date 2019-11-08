5a1c003e2416c43084f2439119dfac2ac2f7a878
Leilani A Trautman <trautman@mit.edu>
2019-11-08 14:44:40 -0500

    added camera code

--- sos/sos.srcs/sources_1/new/servo.sv
+++ sos/sos.srcs/sources_1/new/servo.sv
@@ -1,26 +1,8 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/06/2019 02:03:42 PM
-// Design Name: 
-// Module Name: servo
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
+module servo(angle, servo_pulse);
+    input [7:0] angle;
+    output logic servo_pulse;
 
 
-module servo(
 
-    );
 endmodule
