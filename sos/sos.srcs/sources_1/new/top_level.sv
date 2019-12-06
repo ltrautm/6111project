88f032591a4384b4cce9f0255f06c349a8aa0b1d
Leilani A Trautman <trautman@mit.edu>
2019-12-05 23:01:10 -0500

    servo sweeps left and right kind of jankily, but it works

--- sos/sos.srcs/sources_1/new/top_level.sv
+++ sos/sos.srcs/sources_1/new/top_level.sv
@@ -19,7 +19,7 @@ module top_level(
 //    clk_wiz_lab3 clkdivider(.clk_in1(clk_100mhz), .clk_out1(clk_65mhz));
     
     
-    logic [7:0] myang = 8'd270;
+    logic [7:0] myang = 8'd255;
     
     logic clk_50mhz = 1'b0;
     
@@ -36,6 +36,26 @@ module top_level(
     end
         
 //    clk_wiz_0 clkmulti(.clk_in1(clk_100mhz), .clk_out1(clk_50mhz));
+
+
+    //code to make servo sweep
+    logic [27:0] hz_count = 28'b0;
+    logic mydir = 0; //direction of the servo, 0 is left, 1 is right
+    always_ff @(posedge clk_100mhz) begin
+        if (hz_count == 28'd50000000) begin
+            hz_count <= 28'b0;
+            
+            if (myang >= 8'd250) mydir <= 0;
+            else if (myang <= 8'd5) mydir <= 5;
+            
+            if (mydir == 0) myang <= myang-28'd5;
+            else if (mydir == 1) myang <= myang +28'd5;
+        end else begin
+            hz_count <= hz_count+28'b1;
+        end
+    
+    end
+
 
     servo_controller mysc(.clk(clk_50mhz),
                             .rst(1'b0),
