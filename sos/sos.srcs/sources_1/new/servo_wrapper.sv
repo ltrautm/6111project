40e62c4ae5378c96418a4451d30875ef2b585040
Leilani <leilanitrautman@gmail.com>
2019-12-09 13:38:05 -0500

    added the servo wrapper

--- sos/sos.srcs/sources_1/new/servo_wrapper.sv
+++ sos/sos.srcs/sources_1/new/servo_wrapper.sv
@@ -11,18 +11,20 @@
 
 module servo_wrapper(
    input clk, //200mhz
+   input servo_dir,
+   input servo_stop,
    output logic js //outputting the serial here
    );
    
 //    logic clk_65mhz;
     // create 65mhz system clock, happens to match 1024 x 768 XVGA timing
 //    clk_wiz_lab3 clkdivider(.clk_in1(clk_100mhz), .clk_out1(clk_65mhz));
-    
-    
-    logic [7:0] myang = 8'd255;
-    
+   
+   
+    logic [7:0] myang = 8'd127;
+   
     logic clk_50mhz = 1'b0;
-    
+   
     logic [2:0]  county = 3'b0;
     always_ff @(posedge clk) begin
         if (county == 3'd4) begin
@@ -30,30 +32,34 @@ module servo_wrapper(
             clk_50mhz <= 1'b1;
         end else begin
             county <= county + 4'd1;
-            clk_50mhz <= 1'b0; 
+            clk_50mhz <= 1'b0;
         end
-    
+   
     end
-        
+       
 //    clk_wiz_0 clkmulti(.clk_in1(clk_100mhz), .clk_out1(clk_50mhz));
 
 
     //code to make servo sweep
     logic [27:0] hz_count = 28'b0;
-    logic mydir = 0; //direction of the servo, 0 is left, 1 is right
+//    logic mydir = 0; //direction of the servo, 0 is left, 1 is right
     always_ff @(posedge clk) begin
-        if (hz_count == 28'd100000000) begin
-            hz_count <= 28'b0;
-            
-            if (myang >= 8'd250) mydir <= 0;
-            else if (myang <= 8'd5) mydir <= 1;
-            
-            if (mydir == 0) myang <= myang-28'd5;
-            else if (mydir == 1) myang <= myang +28'd5;
+        if (servo_stop == 1'b0) begin
+            if (hz_count == 28'd50_000_000) begin
+                hz_count <= 28'b0;
+               
+//                if (myang >= 8'd250) mydir <= 0;
+//                else if (myang <= 8'd5) mydir <= 1;
+               
+                if (servo_dir == 1'b0 && myang > 8'd5) myang <= myang-28'd5;
+                else if (servo_dir == 1'b1 && myang < 8'd250) myang <= myang +28'd5;
+            end else begin
+                hz_count <= hz_count+28'd1;
+            end
         end else begin
-            hz_count <= hz_count+28'd1;
+            hz_count <= 28'd0;
         end
-    
+   
     end
 
 
@@ -61,13 +67,12 @@ module servo_wrapper(
                             .rst(1'b0),
                             .position(myang),
                             .servo(js));
-    
+   
 //    servo my_servo(.clk(clk_100mhz), .angle(myang), .servo_pulse(jc[0]));    
-    
+   
 //    always_ff @(posedge clk_65mhz) begin
-        
-            
+       
+           
 //    end
 
 endmodule
-
